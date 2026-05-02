00fe42c9f32aabb13bdae60ce7516c4e4a540e1f
ayushgaur475 <ayushgaur475@gmail.com>
2026-05-03 01:42:26 +0530

    fix: revert to stable payment methods

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -97,7 +97,7 @@ const placeOrder = async (req,res) => {
         const session = await stripe.checkout.sessions.create({
             line_items:line_items,
             mode:'payment',
-            payment_method_types: ['card', 'upi', 'netbanking'],
+            payment_method_types: ['card', 'upi'],
             success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
             cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
         })
