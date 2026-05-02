d52cf589799263fc442b6e451fb34de38cca4568
ayushgaur475 <ayushgaur475@gmail.com>
2026-05-03 02:06:49 +0530

    fix: allow admin panel in CORS whitelist

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5001;
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ["https://khaanpaan-frontend.vercel.app", "http://localhost:5173"],
+    origin: ["https://khaanpaan-frontend.vercel.app", "http://localhost:5173", "http://localhost:5174"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
