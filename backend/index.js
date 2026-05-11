765ffdc6ff4817eb058356912d74ca9e2d0f00e8
ayushgaur475 <ayushgaur475@gmail.com>
2026-05-11 17:10:02 +0530

    Ensure environment variables are loaded first for Firebase Admin

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { connectDB, getGridFSBucket } from './config/db.js';
 import foodRouter from './routes/foodRoutes.js';
 import userRouter from './routes/userRoute.js';
-import 'dotenv/config';
 import cartRouter from './routes/cartRoutes.js';
 import orderRouter from './routes/orderRoute.js';
 
